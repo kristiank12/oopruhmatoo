4b438c62ad4335d786db4fead9c93b88ba6404b1
tudengm <magnarkaarik@ut.ee>
2026-04-02 01:28:32 +0300

    ajalugu sisse pandud

--- src/kalkulaator.java
+++ src/kalkulaator.java
@@ -1,70 +1,99 @@
+import java.util.ArrayList;
 import java.util.InputMismatchException;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class kalkulaator {
     public static void main(String[] args) {
-        System.out.println("Kalkulaatori tegevused: lahutamine (-), liitmine (+), korrutamine (*), jagamine (*), suvalise arvu tagastamine lõigust (r), sulgemine (q)");
+        System.out.println("Kalkulaatori tegevused: lahutamine (-), liitmine (+), korrutamine (*), jagamine (*), suvalise arvu tagastamine lõigust (r), tehete ajaloo vaatamine (h), sulgemine (q)");
         Scanner kasutajaSisend = new Scanner(System.in);
         kasutajaSisend.useLocale(Locale.ENGLISH); // Loeb murdarve punktiga, mitte komaga
         sisendiHaldur sisendiHaldur = new sisendiHaldur(kasutajaSisend);
 
+        //Kalkulaatori ajaloo salvestamine
+        Ajalugu logi = new Ajalugu();
+        ArrayList<String> ajalooPuhver = new ArrayList<>(); //arvude salvestamiseks
+
+        int arvePraegu = 0;
         // Kasutaja kalkulaatori loop
         do {
-                System.out.println("Sisesta arv või tehte märk: ");
-                String tüüp = sisendiHaldur.määraTüüp();
-                String sisend = kasutajaSisend.nextLine();
+            System.out.println("Sisesta arv või tehte märk: ");
+            String tüüp = sisendiHaldur.määraTüüp();
+            String sisend = kasutajaSisend.nextLine();
+            double arv = 0;
 
-                if (tüüp.equals("String")) {
-                    if (sisend.equals("q")) {
-                        kasutajaSisend.close();
-                        break;
+            if (tüüp.equals("String")) {
+                if (sisend.equals("q")) {
+                    kasutajaSisend.close();
+                    break;
+                    //ajaloo kuvamine sisendiga h või ajalugu (juhises ainult h)
+                } else if (sisend.equals("h") || sisend.equals("ajalugu")) {
+                    System.out.println("Tehete ajalugu: ");
+                    System.out.println(logi.koik());
+
                     // Märgi kontrollimine
-                    } else if (sisend.equals("+")) {
-                        if (sisendiHaldur.arvePraegu() > 1) {
-                            System.out.println("Tehte vastus on " + sisendiHaldur.arvuta("+").getVaartus());
-                            sisendiHaldur.eemaldaArvud();
-                        } else {
-                            System.out.println("Pole antud piisavalt arve.");
-                        }
-                    } else if (sisend.equals("-")) {
-                        if (sisendiHaldur.arvePraegu() > 1) {
-                            System.out.println("Tehte vastus on " + sisendiHaldur.arvuta("-").getVaartus());
-                            sisendiHaldur.eemaldaArvud();
-                        } else {
-                            System.out.println("Pole antud piisavalt arve.");
-                        }
-                    } else if (sisend.equals("*")) {
-                        if (sisendiHaldur.arvePraegu() > 1) {
-                            System.out.println("Tehte vastus on " + sisendiHaldur.arvuta("*").getVaartus());
-                            sisendiHaldur.eemaldaArvud();
-                        } else {
-                            System.out.println("Pole antud piisavalt arve.");
-                        }
-                    } else if (sisend.equals("/")) {
-                        if (sisendiHaldur.arvePraegu() > 1) {
-                                System.out.println("Tehte vastus on " + sisendiHaldur.arvuta("/").getVaartus());
-                                sisendiHaldur.eemaldaArvud();
-                        } else {
-                            System.out.println("Pole antud piisavalt arve.");
-                        }
-                    } else if (sisend.equals("r")) {
-                        if (sisendiHaldur.arvePraegu() > 1) {
-                            System.out.println("Suvaline arv on " + sisendiHaldur.arvuta("r").getVaartus());
-                            sisendiHaldur.eemaldaArvud();
-                        } else {
-                            System.out.println("Pole antud piisavalt arve.");
-                        }
+                } else if (sisend.equals("+")) {
+                    if (sisendiHaldur.arvePraegu() > 1) {
+                        double vastus = sisendiHaldur.arvuta("+").getVaartus();
+                        logi.salvesta(String.join(" + ", ajalooPuhver), vastus);
+                        ajalooPuhver.clear(); //ajaloo salvestamine
+                        System.out.println("Tehte vastus on " + vastus);
+                        sisendiHaldur.eemaldaArvud();
                     } else {
-                        System.out.println("Pole õige sisestus.");
+                        System.out.println("Pole antud piisavalt arve.");
                     }
-                } else if (tüüp.equals("Int")) {
-                    sisendiHaldur.lisaArv(new MyInteger(Integer.parseInt(sisend)));
-                } else if (tüüp.equals("Double")) {
-                    sisendiHaldur.lisaArv(new MyDouble(Double.parseDouble(sisend)));
+                } else if (sisend.equals("-")) {
+                    if (sisendiHaldur.arvePraegu() > 1) {
+                        double vastus = sisendiHaldur.arvuta("-").getVaartus();
+                        logi.salvesta(String.join(" - ", ajalooPuhver), vastus);
+                        ajalooPuhver.clear();
+                        System.out.println("Tehte vastus on " + vastus);
+                        sisendiHaldur.eemaldaArvud();
+                    } else {
+                        System.out.println("Pole antud piisavalt arve.");
+                    }
+                } else if (sisend.equals("*")) {
+                    if (sisendiHaldur.arvePraegu() > 1) {
+                        double vastus = sisendiHaldur.arvuta("*").getVaartus();
+                        logi.salvesta(String.join(" * ", ajalooPuhver), vastus);
+                        ajalooPuhver.clear();
+                        System.out.println("Tehte vastus on " + vastus);
+                        sisendiHaldur.eemaldaArvud();
+                    } else {
+                        System.out.println("Pole antud piisavalt arve.");
+                    }
+                } else if (sisend.equals("/")) {
+                    if (sisendiHaldur.arvePraegu() > 1) {
+                        double vastus = sisendiHaldur.arvuta("/").getVaartus();
+                        logi.salvesta(String.join(" / ", ajalooPuhver), vastus);
+                        ajalooPuhver.clear();
+                        System.out.println("Tehte vastus on " + vastus);
+                        sisendiHaldur.eemaldaArvud();
+                    } else {
+                        System.out.println("Pole antud piisavalt arve.");
+                    }
+                } else if (sisend.equals("r")) {
+                    if (sisendiHaldur.arvePraegu() > 1) {
+                        double vastus = sisendiHaldur.arvuta("r").getVaartus();
+                        logi.salvesta("Suvaline arv vahemikus " + ajalooPuhver.get(0) + " kuni " + ajalooPuhver.get(1), vastus);
+                        ajalooPuhver.clear();
+                        System.out.println("Suvaline arv on " + vastus);
+                        sisendiHaldur.eemaldaArvud();
+                    } else {
+                        System.out.println("Pole antud piisavalt arve.");
+                    }
+                } else {
+                    System.out.println("Pole õige sisestus.");
                 }
+            } else if (tüüp.equals("Int")) {
+                arv = (double) Integer.parseInt(sisend);
+                sisendiHaldur.lisaArv(new MyInteger(Integer.parseInt(sisend)));
+                ajalooPuhver.add(sisend);
+            } else if (tüüp.equals("Double")) {
+                sisendiHaldur.lisaArv(new MyDouble(Double.parseDouble(sisend)));
+                ajalooPuhver.add(sisend);
+            }
 
         } while (true);
     }
-
 }
